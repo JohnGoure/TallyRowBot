bbd23c0afa527ace7e26edd7a1852a7c38aa180c
John Goure <johnsgithub@gmail.com>
2017-12-11 22:45:11 -0600

    initial commit

---
+++ ircbot.py
@@ -0,0 +1,134 @@
+import sys
+
+import irc.bot
+import requests
+
+from player import *
+
+
+class TwitchBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, username, client_id, token, channel):
+        self.client_id = client_id
+        self.token = token
+        self.channel = '#' + channel
+        self._hollowKnightBot = Player()
+
+        # Get the channel id, we will need this for v5 API calls
+        url = 'https://api.twitch.tv/kraken/users?login=' + channel
+        headers = {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        r = requests.get(url, headers=headers).json()
+        self.channel_id = r['users'][0]['_id']
+
+        # Create IRC bot connection
+        server = 'irc.chat.twitch.tv'
+        port = 6667
+        print('Connecting to ' + server + ' on port ' + str(port) + '...')
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:' + token)], username, username)
+
+    def on_welcome(self, c, e):
+        print('Joining ' + self.channel)
+
+        # You must request specific capabilities before you can use them
+        c.cap('REQ', ':twitch.tv/membership')
+        c.cap('REQ', ':twitch.tv/tags')
+        c.cap('REQ', ':twitch.tv/commands')
+        c.join(self.channel)
+        print('Joined ' +self.channel)
+
+    def on_pubmsg(self, c, e):
+
+        # If a chat message starts with an exclamation point, try to run it as a command
+        if e.arguments[0][:1] == '!':
+            cmd = e.arguments[0].split(' ')[0][1:]
+            print('Received command: ' + cmd)
+            self.do_command(e, cmd)
+        return
+
+    def do_command(self, e, cmd):
+        c = self.connection
+
+        # Poll the API to get current game.
+        if cmd == "game":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' is currently playing ' + r['game'])
+
+        # Poll the API the get the current status of the stream
+        elif cmd == "title":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' channel title is currently ' + r['status'])
+
+        # Provide basic information to viewers for specific commands
+        elif cmd == "raffle":
+            message = "This is an example bot, replace this text with your raffle text."
+            c.privmsg(self.channel, message)
+        elif cmd == "schedule":
+            message = "This is an example bot, replace this text with your schedule text."
+            c.privmsg(self.channel, message)
+
+        # Provide player movement
+        elif cmd == "up":
+            self._hollowKnightBot.lookUp()
+        elif cmd == "down":
+            self._hollowKnightBot.lookDown()
+        elif cmd == "right":
+            self._hollowKnightBot.shortStepRight()
+        elif cmd == "longright":
+            self._hollowKnightBot.longStepRight()
+        elif cmd == 'left':
+            self._hollowKnightBot.shortStepLeft()
+        elif cmd == 'longleft':
+            self._hollowKnightBot.longStepLeft()
+        elif cmd == 'jump':
+            self._hollowKnightBot.jump()
+        elif cmd == 'longjump':
+            self._hollowKnightBot.longJump()
+        elif cmd == 'dash':
+            self._hollowKnightBot.dash()
+        elif cmd == 'superdash':
+            self._hollowKnightBot.superDash()
+        elif cmd == 'inventory':
+            self._hollowKnightBot.inventory()
+        elif cmd == 'attack':
+            self._hollowKnightBot.attack()
+        elif cmd == 'stop':
+            self._hollowKnightBot.stop()
+        elif cmd == 'holdright':
+            self._hollowKnightBot.holdRight()
+        elif cmd == 'holdleft':
+            self._hollowKnightBot.holdLeft()
+        elif cmd == 'upattack':
+            self._hollowKnightBot.upAttack()
+        elif cmd == 'downattack':
+            self._hollowKnightBot.downAttack()
+        elif cmd == 'select':
+            self._hollowKnightBot.select()
+        elif cmd == 'quickcast':
+            self._hollowKnightBot.quickCast()
+        elif cmd == 'quickmap':
+            self._hollowKnightBot.quickMap()
+
+        # The command was not recognized
+        else:
+            c.privmsg(self.channel, "Did not understand command: " + cmd)
+
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: twitchbot <username> <client id> <token> <channel>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    client_id = sys.argv[2]
+    token = sys.argv[3]
+    channel = sys.argv[4]
+
+    bot = TwitchBot(username, client_id, token, channel)
+    bot.start()
+
+
+if __name__ == "__main__":
+    main()
