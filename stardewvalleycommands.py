0bd342febc5af36c89220e84c52be432f67ac21f
John Goure <johnsgithub@gmail.com>
2017-12-12 18:24:56 -0600

    Added the stardew valley commands class.

---
+++ stardewvalleycommands.py
@@ -0,0 +1,38 @@
+import pyautogui
+
+pyautogui.PAUSE =0
+pyautogui.FAILSAFE = True
+
+class StardewValleyCommands():
+    """ This class implements the model for Stardew Valley Commands"""
+    # initialize the class with the strings associated with the keyboard commands
+    def __init__(self):
+        self._up = 'w'
+        self._down = 's'
+        self._left = 'a'
+        self._right = 'd'
+        self._map = 'm'
+        self._menu = 'e'
+        self._journal = 'f'
+        self._leftclick = 'c'
+        self._rightclick = 'x'
+
+    def moveUp(self): #Press the key to move up up
+
+    def moveDown(self): # Press the key to move down
+
+    def moveLeft(self): # Press the key to move left
+
+    def moveRight(self): #Press the key to move right
+        
+    def openMap(self): #Press the key to open the map
+
+    def openMenu(self): #Press the key to open the menu
+
+    def openJournal(self): #Press the key to open the journal
+
+    def leftClick(self): #Press the key to left click
+
+    def rightClick(self): #Press the key to right click
+
+
